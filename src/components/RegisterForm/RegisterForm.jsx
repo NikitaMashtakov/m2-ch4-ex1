d582d874fe2b40bb4be5845f7f0385a84eea14a4
Nikita Mashtakov <n-mashtakov@yandex.ru>
2025-04-04 17:56:15 +0700

    add controllers

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,119 +1,86 @@
-import { useState, useRef, useEffect } from 'react';
 import { Input } from '../Input/Input';
 import { Button } from '../Button/Button';
 import styles from './RegisterForm.module.css';
+import { Controller, useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+const schema = yup
+  .object({
+    email: yup.string().email().required('Введите e-mail'),
+    password: yup.string().min(8).required('Введите пароль'),
+  })
+  .required();
 
 export const RegisterForm = () => {
-  const [formData, setFormData] = useState({
-    email: '',
-    password: '',
-    repeatPassword: '',
+  const {
+    control,
+    handleSubmit,
+    trigger,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      email: '',
+      password: '',
+      repeatPassword: '',
+    },
+    resolver: yupResolver(schema),
   });
-  const [formErrors, setFormErrors] = useState({
-    emailError: null,
-    passwordError: null,
-    repeatPasswordError: null,
-  });
-  const [submitBtnDisabled, setSubmitBtnDisabled] = useState(true);
-  const submitButtonRef = useRef(null);
-
-  useEffect(() => {
-    const canSubmit =
-      Object.values(formErrors).some((value) => value !== null) ||
-      Object.values(formData).some((value) => value === '');
-    if (canSubmit) {
-      setSubmitBtnDisabled(true);
-    } else {
-      setSubmitBtnDisabled(false);
-    }
-  }, [formData, formErrors]);
-
-  const onEmailChange = ({ target }) => {
-    setFormData((prev) => ({ ...prev, email: target.value }));
-  };
-
-  const onEmailBlur = ({ target }) => {
-    let newError = null;
-
-    if (target.value.length < 1) {
-      newError = 'Адрес не должен быть пустым.';
-    } else if (!/^[\w.-]+@([\w-]+\.)+[\w-]{2,4}$/.test(target.value)) {
-      newError =
-        'Неверный адрес почты. Адрес должен соответствовать виду example@example.com';
-    }
-    setFormErrors((prev) => ({ ...prev, emailError: newError }));
-  };
-
-  const onPasswordChange = ({ target }) => {
-    setFormData((prev) => ({ ...prev, password: target.value }));
-  };
-
-  const onPasswordBlur = ({ target }) => {
-    let newError = null;
-
-    if (target.value.length < 1) {
-      newError = 'Введите пароль';
-    }
-    setFormErrors((prev) => ({ ...prev, passwordError: newError }));
-  };
-
-  const onRepeatPasswordChange = ({ target }) => {
-    setFormData((prev) => ({ ...prev, repeatPassword: target.value }));
-    if (formData.password === target.value) {
-      setFormErrors((prev) => ({ ...prev, repeatPasswordError: null }));
-      if (formErrors.emailError === null) {
-        submitButtonRef.current.focus();
-      }
-    }
-  };
-
-  const onRepeatPasswordBlur = ({ target }) => {
-    if (formData.password !== target.value) {
-      setFormErrors((prev) => ({ ...prev, repeatPasswordError: 'Пароли не совпадают' }));
-    } else {
-      setFormErrors((prev) => ({ ...prev, repeatPasswordError: null }));
-    }
-  };
-
-  const submitForm = () => {
-    const { email, password } = formData;
-    console.log(email, password);
+  const submitForm = (data) => {
+    console.log(data);
   };
 
   return (
     <div className={styles.container}>
-      <form className={styles.registerForm} action={submitForm}>
-        <Input
-          type="email"
+      <form className={styles.registerForm} onSubmit={handleSubmit(submitForm)}>
+        <Controller
           name="email"
-          placeholder="example@example.com"
-          label="E-mail"
-          value={formData.email}
-          onChange={onEmailChange}
-          onBlur={onEmailBlur}
-          error={formErrors.emailError}
+          control={control}
+          render={({ field }) => (
+            <Input
+              type="email"
+              placeholder="example@example.com"
+              label="E-mail"
+              error={errors.email?.message}
+              {...field}
+            />
+          )}
         />
-        <Input
-          type="password"
+
+        <Controller
           name="password"
-          placeholder="Введите пароль"
-          label="Пароль"
-          value={formData.password}
-          onChange={onPasswordChange}
-          onBlur={onPasswordBlur}
-          error={formErrors.passwordError}
+          control={control}
+          render={({ field }) => (
+            <Input
+              type="password"
+              placeholder="Введите пароль"
+              label="Пароль"
+              error={errors.password?.message}
+              {...field}
+            />
+          )}
         />
-        <Input
-          type="password"
+
+        <Controller
           name="repeatPassword"
-          placeholder="Повторите пароль"
-          label="Пароль"
-          value={formData.repeatPassword}
-          onChange={onRepeatPasswordChange}
-          onBlur={onRepeatPasswordBlur}
-          error={formErrors.repeatPasswordError}
+          control={control}
+          render={({ field }) => (
+            <Input
+              type="password"
+              placeholder="Повторите пароль"
+              label="Пароль"
+              error={errors.repeatPassword?.message}
+              {...field}
+            />
+          )}
         />
-        <Button ref={submitButtonRef} type="submit" disabled={submitBtnDisabled}>
+
+        <Button
+          type="submit"
+          onClick={() => {
+            trigger();
+          }}
+        >
           Зарегистрироваться
         </Button>
       </form>
